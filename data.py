7e5ef43d547c8e7c59042da05374a26d9fedfb02
fente <mickeyfent3@gmail.com>
2025-04-13 00:57:21 +0000

    request for api working

--- data.py
+++ data.py
@@ -1,8 +1,14 @@
+from xmlrpc.client import boolean
+
 import requests
 
 parameters ={
-
+    "amount": 10,
+    "type": "boolean"
 }
 
-response = requests.get("https://opentdb.com/api", params=)
-question_data = []
+response = requests.get("https://opentdb.com/api.php", params= parameters)
+response.raise_for_status()
+data = response.json()
+# print(data)
+question_data = data["results"]
