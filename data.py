bebfc675cd6228d8cbfe3a950997afda6bf159d4
fente <mickeyfent3@gmail.com>
2025-04-14 12:26:54 +0000

    request for api working

--- data.py
+++ data.py
@@ -7,6 +7,8 @@ parameters ={
     "type": "boolean"
 }
 
+
+#Get API and add your parameters
 response = requests.get("https://opentdb.com/api.php", params= parameters)
 response.raise_for_status()
 data = response.json()
