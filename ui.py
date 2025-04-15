4b6869098d467907cac38b26fe6037da83c9f8d3
fente <mickeyfent3@gmail.com>
2025-04-15 20:06:00 +0000

    Started developing the ui using tkinter

--- ui.py
+++ ui.py
@@ -4,4 +4,12 @@ from tkinter import *
 THEME_COLOR = "#375362"
 
 windows = Tk()
-windows.wait_window()
+windows.title("quizzler-app")
+
+windows.geometry("400x500")  # Starting size
+windows.minsize(300, 400)
+windows.config(padx=20, pady=20)
+
+
+
+windows.mainloop()
