68afe503e528ede1ccd375261e5c5c008b8a7269
fente <mickeyfent3@gmail.com>
2025-04-16 20:11:18 +0000

    Started developing the ui using tkinter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
@@ -12,9 +13,11 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
+quiz_ui.score_board.grid()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 
 print("You've completed the quiz")
--- ui.py
+++ ui.py
@@ -3,13 +3,27 @@ from tkinter import *
 
 THEME_COLOR = "#375362"
 
-windows = Tk()
-windows.title("quizzler-app")
 
-windows.geometry("400x500")  # Starting size
-windows.minsize(300, 400)
-windows.config(padx=20, pady=20)
+class QuizInterface:
+    def __init__(self):
+        self.windows = Tk()
+        self.windows.title("Quizzler")
+        self.windows.geometry("400x500")  # Starting size
+        self.windows.minsize(300, 400)
+        self.windows.config(padx=20, pady=20, bg=THEME_COLOR)
+
+
+        self.score_board = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_board.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=500, bg="white")
+        self.text_display =self.canvas.create_text(text="Some of the Question",)
 
 
 
-windows.mainloop()
+
+
+
+
+
+        self.windows.mainloop()
