3a470249dad3af3c157d556a1762cc680ba20003
fente <mickeyfent3@gmail.com>
2025-04-21 23:40:47 +0000

    modifying the ui

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ for question in question_data:
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
 quiz_ui = QuizInterface()
 quiz_ui.score_board.grid()
--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
@@ -17,11 +18,23 @@ class QuizInterface:
         self.score_board.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=500, bg="white")
-        self.text_display =self.canvas.create_text(text="Some of the Question",)
-        self.canvas.grid()
+        self.text_display =self.canvas.create_text(
+            150,
+            250,
+            text=f"Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial",20,"italic")
 
+        )
+        self.canvas.grid(row= 1 ,column= 0, columnspan=2, pady=50)
 
+        true_image= PhotoImage(file="images/true.png")
+        self.true_button =  Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
 
+        false_image= PhotoImage(file="images/false.png")
+        self.false_button =  Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
 
 
